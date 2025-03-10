358d1b927be12384caf522109d21328aa02b3c58
Zane LaBute <zlabute@g.ucla.edu>
2025-03-09 23:57:01 -0700

    untested ooo.cpp implementation

--- src/ooo.cpp
+++ src/ooo.cpp
@@ -32,7 +32,21 @@ void Core::issue() {
   auto exe_flags = instr->getExeFlags();
 
   // check for structial hazards
-  // TODO:
+  // TODO: DONE
+  int free_rs_index = -1;
+  for (int i = 0; i < (int)RS_.size(); i++) 
+  {
+    auto& entry = RS_.get_entry(i);
+    if (!entry.valid) 
+    {
+      free_rs_index = i;
+      break;
+    }
+  }
+  if (free_rs_index == -1) 
+  {
+    return;
+  }
 
   uint32_t rs1_data = 0;  // rs1 data obtained from register file or ROB
   uint32_t rs2_data = 0;  // rs2 data obtained from register file or ROB
@@ -48,7 +62,27 @@ void Core::issue() {
   // else set rs1_rsid to the reservation station id producing the data
   // remember to first check if the instruction actually uses rs1
   // HINT: should use RAT, ROB, RST, and reg_file_
-  // TODO:
+  // TODO: Done
+  if (exe_flags.use_rs1)
+  {
+    if (!RAT_.exists(rs1)) 
+    {
+      rs1_data = reg_file_.at(rs1);
+    }
+    else {
+      int rob_idx = RAT_.get(rs1);
+      auto& rob_entry = ROB_.get_entry(rob_idx);
+      if (rob_entry.ready)
+      {
+        rs1_data = rob_entry.result;
+      }
+      else
+      {
+        rs1_rsid = rob_idx;
+      }
+    }
+  }
+
 
   // get rs2 data
   // check the RAT if value is in the registe file
@@ -56,19 +90,60 @@ void Core::issue() {
   // else set rs1_rsid to the reservation station id producing the data
   // remember to first check if the instruction actually uses rs2
   // HINT: should use RAT, ROB, RST, and reg_file_
-  // TODO:
+  // TODO: Done
+  if (exe_flags.use_rs2) {
+    if (!RAT_.exists(rs2))
+    {
+      rs2_data = reg_file_.at(rs2);
+    } 
+    else 
+    {
+      int rob_idx = RAT_.get(rs2);
+      auto& rob_entry = ROB_.get_entry(rob_idx);
+      if (rob_entry.ready) 
+      {
+        rs2_data = rob_entry.result;
+      } 
+      else 
+      {
+        rs2_rsid = rob_idx;
+      }
+    }
+  }
 
-  // allocat new ROB entry and obtain its index
-  // TODO:
+  // allocate new ROB entry and obtain its index
+  // TODO: DONE
+  int rob_index = ROB_.allocate(instr);
 
   // update the RAT mapping if this instruction write to the register file
-  // TODO:
+  // TODO: DONE
+  if (exe_flags.use_rd) 
+  {
+    int rd = instr->getRd();
+    RAT_.set(rd, rob_index);
+  }
 
   // issue the instruction to free reservation station
-  // TODO:
+  // TODO: DONE
+  auto& rs_entry = RS_.get_entry(free_rs_index);
+  rs_entry.valid = true;
+  rs_entry.instr = instr;
+  rs_entry.rob_index = rob_index;
+  rs_entry.rs1_index = (exe_flags.use_rs1 && rs1_rsid != -1) ? rs1_rsid : -1;
+  rs_entry.rs2_index = (exe_flags.use_rs2 && rs2_rsid != -1) ? rs2_rsid : -1;
+  rs_entry.rs1_data = rs1_data;
+  rs_entry.rs2_data = rs2_data;
+  rs_entry.running = false;
+
 
   // update RST mapping
-  // TODO:
+  // TODO: DONE???????
+  if (exe_flags.use_rd) // unsure if RST_ mapping should be updated within conditional or just regardless because it says update RST mapping but doesn't specicify to check if instruction writes to register file
+  {
+    int rd = instr->getRd();
+    RST_.set(rd, rob_index);
+  }
+
 
   DT(2, "Issue: " << *instr);
 
@@ -88,7 +163,14 @@ void Core::execute() {
   // The CDB can only serve one functional unit per cycle
   // HINT: should use CDB_ and FUs_
   for (auto fu : FUs_) {
-    // TODO:
+    // TODO: DONE
+    if (fu->done()) 
+    {
+      auto out = fu->get_output();
+      CDB_.push(out.result, out.rob_index, out.rs_index);
+      fu->clear();
+      break; // break after found first functional unit done as CDB can only serve one function unit per cycle
+    }
   }
 
   // schedule ready instructions to corresponding functional units
@@ -98,7 +180,19 @@ void Core::execute() {
   // HINT: should use RS_ and FUs_
   for (int rs_index = 0; rs_index < (int)RS_.size(); ++rs_index) {
     auto& entry = RS_.get_entry(rs_index);
-    // TODO:
+    // TODO: DONE
+    if (entry.valid && !entry.running && entry.rs1_index == -1 && entry.rs2_index == -1)
+    {
+      if (entry.instr->getFUType() == FUType::LSU && RS_.locked(rs_index))
+      {
+        continue;
+      }
+      FUType fu_type = entry.instr->getFUType();
+      auto fu = FUs_.at((int)fu_type);
+
+      fu->issue(entry.instr, entry.rob_index, rs_index, entry.rs1_data, entry.rs2_data);
+      entry.running = true;
+    }
   }
 }
 
@@ -112,18 +206,38 @@ void Core::writeback() {
   // update all reservation stations waiting for operands
   // HINT: use RS::entry_t::update_operands()
   for (int rs_index = 0; rs_index < (int)RS_.size(); ++rs_index) {
-    // TODO:
+    // TODO: DONE
+    auto& entry = RS_.get_entry(rs_index);
+    if (entry.valid)
+    {
+      entry.update_operands(cdb_data);
+    }
   }
 
   // free the RS entry associated with this CDB response
   // so that it can be used by other instructions
-  // TODO:
+  // TODO: DONE
+  for (int rs_index = 0; rs_index < (int)RS_.size(); rs_index++)
+  {
+    auto& entry = RS_.get_entry(rs_index);
+    if (entry.valid && entry.rob_index == cdb_data.rob_index) 
+    {
+      entry.valid = false;
+      int rd = entry.instr->getRd();
+      if (RST_.exists(rd) && RST_.get(rd) == cdb_data.rob_index){
+        RST_.clear(rd);
+      }
+      break;
+    }
+  }
 
   // update ROB
-  // TODO:
+  // TODO: DONE
+  ROB_.update(cdb_data);
 
   // clear CDB
-  // TODO:
+  // TODO: DONE
+  CDB_.pop();
 
   RS_.dump();
 }
@@ -144,10 +258,19 @@ void Core::commit() {
     // If this instruction writes to the register file,
     // (1) update the register file
     // (2) clear the RAT if still pointing to this ROB head
-    // TODO:
+    // TODO: DONE
+    if (exe_flags.use_rd)
+    {
+      int rd = instr->getRd();
+      reg_file_.at(rd) = rob_head.result;
+      if (RAT_.exists(rd) && RAT_.get(rd) == head_index){
+        RAT_.clear(rd);
+      }
+    }
 
     // pop ROB entry
     // TODO:
+    ROB_.pop();
 
     DT(2, "Commit: " << *instr);
 
